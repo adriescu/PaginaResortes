cca2c8ea21a5b449204fc4d85fff547f04940ae4
adriescu <95098450+adriescu@users.noreply.github.com>
2022-10-04 22:15:26 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/index2.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script defer src="js/index.js"></script>
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <a href="" class="logo nav-link">Resortes</a>
+            <button class="nav-toggle">
+            <i class="material-icons">menu</i>
+            </button>
+            <ul class="nav-menu">
+                <li class="nav-menu-item">
+                    <a href="#" class="nav-menu-link nav-link nav-menu-link_active">Inicio</a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#" class="nav-menu-link nav-link">Hacer pedido</a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#" class="nav-menu-link nav-link">Catálogo</a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#" class="nav-menu-link nav-link">Preguntas frecuentes</a>
+                </li>
+                <li class="nav-menu-item">
+                    <a href="#" class="nav-menu-link nav-link">Acerca de</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="main">
+        <div class="main-div-principal">
+            <h1>Resortes lorem ipsum</h1>
+            <p>Esto es información importante lorem ipsum</p>
+            <p class="boton-estilo bg-secundario">Iniciar Sesión</p>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-logo footer-items">
+                <img src="img/resorte.png" alt="" width="30px">
+            </div>
+            <div class="footer-info footer-items">
+            <h3>Info</h3>
+                <ul>
+                    <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam deleniti quam a inventore, voluptates animi consectetur modi, deserunt consequatur assumenda dignissimos esse enim eveniet eius blanditiis aspernatur ipsam non harum.</p></li>
+                    <li><p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi quis minima sint placeat laboriosam dicta a tempora optio impedit aperiam ipsum ex doloribus, quod sed maxime numquam repudiandae labore? Rerum?</p></li>
+                </ul>
+            </div>
+            <div class="footer-redes footer-items">
+                <h3>Siguenos</h3>
+                <ul>
+                    <li>Instagram</li>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Youtube</li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <p>©2022 <span style="font-weight: bold;">Resortes</span></p>
+        </div>
+    </footer>
+</body>
+</html>
