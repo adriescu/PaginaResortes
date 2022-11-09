d098ed2d67eb5bf37e1d9d53f4643f1804eeda02
adriescu <95098450+adriescu@users.noreply.github.com>
2022-11-09 09:14:27 -0300

    hacer pedido

--- hacerPedido.php
+++ hacerPedido.php
@@ -14,18 +14,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacer Pedido</title>
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/pedidos.css">
 </head>
 <body>
     <?php
         require 'partials/header-empty.php';
     ?>
-
+<div class="formularioPedido">
     <form action="hacerPedido.php" submit="POST">
-        <label for="">Tipo</label>
+        <label for="tipoDeResorte">Tipo</label>
+        <select name="" id="tipoDeResorte">
+            <?php
+                //acá va el código :)
+            ?>
+            <option value="personalizado">Personalizado</option>
+        </select>
+        <label for="">Canatidad</label>
         <input type="text" name="" id="">
-        <label for="">Cantidad</label>
+        <label for="">Medida</label>
+        <input type="number" name="" id="">
+        <label for="">Número de vueltas</label>
+        <input type="number" name="" id="">
+        <label for="">Descripción</label>
         <input type="text" name="" id="">
+        <input type="submit" value="Enviar pedido" id="boton">
     </form>
+</div>
 
     <?php
         require 'partials/footer.php';
